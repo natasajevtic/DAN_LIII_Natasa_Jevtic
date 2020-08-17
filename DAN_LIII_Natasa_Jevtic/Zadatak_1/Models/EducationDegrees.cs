6b36792bde1cf16e4343174b5d8d48dac9ec8ab3
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-17 21:11:00 +0200

    Model classes created

---
+++ DAN_LIII_Natasa_Jevtic/Zadatak_1/Models/EducationDegrees.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadatak_1.Models
+{
+    class EducationDegrees
+    {
+        Dictionary<string, int> levels = new Dictionary<string, int>()
+        {
+            {"I",1 },
+            {"II", 2 },
+            { "III", 3 },
+            { "IV", 4 },
+            { "V", 5 },
+            { "VI", 6 },
+            { "VII", 7 }
+        };
+        /// <summary>
+        /// This method created collection of education degrees.
+        /// </summary>
+        /// <returns>List of education degrees.</returns>
+        public List<string> GetEducationDegree()
+        {            
+            return levels.Keys.ToList();
+        }       
+    }
+}
