b1caf2e27017718baf547922d3c75ad869dc9eb9
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-17 22:31:05 +0200

    Classes for validation created

---
+++ DAN_LIII_Natasa_Jevtic/Zadatak_1/Validations/ExperienceValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Zadatak_1.Validations
+{
+    class ExperienceValidation : ValidationRule
+    {
+        /// <summary>
+        /// This method checks if user enter a positive number or zero for years of experience.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns>ValidationResult.</returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string number = value as string;
+            if (!Int32.TryParse(number, out int numberOfExperience))
+            {
+                return new ValidationResult(false, "Please enter a number.");
+            }
+            else if (numberOfExperience < 0)
+            {
+                return new ValidationResult(false, "Please enter a positive number.");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
